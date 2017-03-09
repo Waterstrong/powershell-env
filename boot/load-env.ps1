e7e5aede652ea3c86f0a7b34649b254542763525
Waterstrong <sqlin@thoughtworks.com>
2017-03-09 23:06:05 +0800

    add load env script

---
+++ boot/load-env.ps1
@@ -0,0 +1,10 @@
+. "$env:SCRIPT_HOME\library\ini-parser.ps1"
+
+$ini = Parse-IniFile "$env:SCRIPT_HOME\home.ini"
+
+$env:PROJECT_HOME = $ini['HOME']['Project']
+$env:JAVA_6_HOME = $ini['HOME']['Java6']
+$env:JAVA_7_HOME = $ini['HOME']['Java7']
+$env:JAVA_8_HOME = $ini['HOME']['Java8']
+$env:JAVA_HOME = $env:JAVA_8_HOME
+.java-path
