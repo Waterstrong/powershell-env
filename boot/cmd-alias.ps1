2edf59c0e719cc4b35e6cfd54d7da1e4b708b852
Waterstrong <sqlin@thoughtworks.com>
2017-03-15 21:12:21 +0800

    create new project folder

--- boot/cmd-alias.ps1
+++ boot/cmd-alias.ps1
@@ -2,4 +2,12 @@ Set-Alias glw ./gradlew
 Set-Alias ll ls
 Set-Alias grep findstr
 
+function Goto-ProjectHome { & cd $env:PROJECT_HOME }
+Set-Alias .project Goto-ProjectHome
+
+function Goto-PsScriptHome { & cd $env:SCRIPT_HOME }
+Set-Alias .ps-script Goto-PsScriptHome
+
 Get-ChildItem "$env:SCRIPT_HOME\source\*.ps1" | Foreach-Object { Set-Alias $_.BaseName $_.FullName }
+
+Get-ChildItem "$env:SCRIPT_HOME\project\*.ps1" | Foreach-Object { Set-Alias ('.'+$_.BaseName) $_.FullName }
